02048c7d7e8e5dbc81d5a1f665effe6c818b60b8
nav-ios <appdev1221@gmail.com>
2023-05-01 14:26:56 +0530

    Initial Commit

---
+++ GitStarUITests/GitStarUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitStarUITestsLaunchTests.swift
+//  GitStarUITests
+//
+//  Created by Nav on 01/05/23.
+//
+
+import XCTest
+
+final class GitStarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
