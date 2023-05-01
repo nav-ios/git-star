c8472aefc0b925c9276cd76793dac62486cbb9b9
nav-ios <appdev1221@gmail.com>
2023-05-01 14:39:14 +0530

    Added representable repo data response in json file in test target

---
+++ GitStar/GitRepo.swift
@@ -0,0 +1,14 @@
+//
+//  GitRepo.swift
+//  GitStar
+//
+//  Created by Nav on 01/05/23.
+//
+
+import Foundation
+struct GitRepo{
+    let name: String
+    let repoDescription: String
+    let repoOwner: String
+    let numberOfStars: Int
+}
